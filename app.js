d732fb0a63df6957a5f6b37c21fe37e0e6ebffd6
DaKheera47 <shaheergoodboy@gmail.com>
2020-07-13 17:42:22 +0500

    Fixing dotenv bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Imported modules
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const app = express();
 const passport = require('passport');
